ea5639e2ee431a41ee89991d7833c2dcb8efef5c
EC2 Default User <ec2-user@ip-172-31-41-237.us-east-2.compute.internal>
2020-04-30 01:51:07 +0000

    combine codes for the leftover cases

--- Add-Two-Nums.py
+++ Add-Two-Nums.py
@@ -1,46 +1,57 @@
+#!/usr/bin/python3
+
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
 
 class Solution:
     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
         res_lst = ListNode(0)
         walker = res_lst
         
-        while l1 and l2:
+        while l1 or l2:
+            x = l1.val if l1 else 0
+            y = l2.val if l2 else 0
             carry = 0
-            tmp_sum = l1.val + l2.val + walker.val
+            tmp_sum = x + y + walker.val
             walker.val = (tmp_sum % 10)
             
             if tmp_sum >= 10:       # for handling carry
                 carry = 1
             
-            if carry == 1 or l1.next or l2.next:        # for different length of nums and if there is carry
+            # moving on
+            if l1: l1 = l1.next
+            if l2: l2 = l2.next
+            
+            # check if there still need to create new node for the result
+            # which means, if there is other numbers need to be added or carry occured
+            if carry or l1 or l2:
                 walker.next = ListNode(carry)           # pass carry first
                 walker = walker.next
+            
+        return res_lst
 
-            # moving on
-            l1 = l1.next
-            l2 = l2.next
-            
-        # leftovers
-        while l1:
-            carry = 0
-            tmp_sum = walker.val + l1.val
-            walker.val = (tmp_sum % 10)
-            
-            if tmp_sum >= 10:       # for handling carry
-                carry = 1
-            
-            if l1.next or carry:         # make new node only if there is number which still needs to be added (from l1)
-                walker.next = ListNode(carry)
-                walker = walker.next
-            l1 = l1.next
+def makeTestLst(lst):
+    res_lst = ListNode(None)
+    walker = res_lst
+    
+    for i in range(len(lst) - 1):
+        walker.val = lst.pop()
+        walker.next = ListNode(None)
+        walker = walker.next
         
-        while l2:
-            carry = 0
-            tmp_sum = walker.val + l2.val
-            walker.val = (tmp_sum % 10)
-            
+    walker.val = lst.pop()
+    return res_lst
+
+if __name__ == "__main__":
+    l1 = makeTestLst([1])
+    l2 = makeTestLst([9,9])
+    
+    s1 = Solution()
+    tmp = s1.addTwoNumbers(l1, l2)
+    while tmp:
+        print(tmp.val)
+        tmp = tmp.next
+    
