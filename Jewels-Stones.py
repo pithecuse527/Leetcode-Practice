7fd249be3ee71f78071b11a19654b1cfc9fb2872
EC2 Default User <ec2-user@ip-172-31-41-237.us-east-2.compute.internal>
2020-05-02 17:15:14 +0000

    add other algorithm using map

--- Jewels-Stones.py
+++ Jewels-Stones.py
@@ -9,4 +9,23 @@ class Solution:
                 cnt += 1
         
         return cnt
-
+        
+    def numJewelsInStones2(self, J: str, S: str) -> int:
+        cnt = 0
+        stone_map = self.returnMap(S)
+        
+        for key in J:
+            if key in stone_map:
+                cnt += stone_map[key]
+        return cnt
+            
+    def returnMap(self, lst):
+        maps = {}
+        
+        for e in lst:
+            if not e in maps:
+                maps[e] = 1
+            else:
+                maps[e] += 1
+                
+        return maps
