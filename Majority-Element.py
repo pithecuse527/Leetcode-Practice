888471bf9ac5f24c52d453f3ef6c769310e40084
EC2 Default User <ec2-user@ip-172-31-41-237.us-east-2.compute.internal>
2020-05-06 15:41:37 +0000

    second version of algorithm using temporary counter

--- Majority-Element.py
+++ Majority-Element.py
@@ -2,19 +2,16 @@
 
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        num_maps = {}
-        majority_num = len(nums) // 2
+        tmp_counter = 0         # checking the num is validate for the candidate of mojority element 
+        candidate = nums[0]
         
         for num in nums:
-            if num not in num_maps:
-                num_maps[num] = 1
+            if num == candidate:
+                tmp_counter += 1
+            elif tmp_counter == 0:
+                candidate = num
             else:
-                num_maps[num] += 1
-                
-        max_majority_num = 0
-        for num in num_maps:
-            if num_maps[num] > majority_num and num_maps[num] >= max_majority_num:
-                max_majority_num = num
+                tmp_counter -= 1
         
-        return max_majority_num
+        return candidate
     
