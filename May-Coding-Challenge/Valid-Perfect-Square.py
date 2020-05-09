9dfe1134703b0d5dd1c5f6c0d3b49a9ff4bd5fea
EC2 Default User <ec2-user@ip-172-31-41-237.us-east-2.compute.internal>
2020-05-09 17:23:23 +0000

    change to iteration

--- May-Coding-Challenge/Valid-Perfect-Square.py
+++ May-Coding-Challenge/Valid-Perfect-Square.py
@@ -2,25 +2,31 @@
 
 class Solution:
     def isPerfectSquare(self, num: int) -> bool:
-        # from math import ceil
         if num == 1: return True
-        tmp = num // 2
-        found = self.binSearch(num, 0, tmp)
+        left = 0
+        right = num // 2
         
-        if found: return True
+        while not left > right:
+            mid = (left + right) // 2
+            mid_square = mid * mid
+            if num == mid_square: return True
+            elif num > mid_square:
+                left = mid + 1
+            else:
+                right = mid - 1
         return False
     
-    def binSearch(self, num: int, left: int, right: int):
-        if left > right: return False
+    # def binSearch(self, num: int, left: int, right: int):
+    #     if left > right: return False
         
-        mid = (left + right) // 2
-        mid_square = mid * mid
+    #     mid = (left + right) // 2
+    #     mid_square = mid * mid
         
-        if num == mid_square: return mid
-        elif num > mid_square: return self.binSearch(num, mid + 1, right)
-        else: return self.binSearch(num, left, mid - 1)
+    #     if num == mid_square: return mid
+    #     elif num > mid_square: return self.binSearch(num, mid + 1, right)
+    #     else: return self.binSearch(num, left, mid - 1)
 
 if __name__ == "__main__":
     sol1 = Solution()
-    print(1 if sol1.isPerfectSquare(4) else 2)
+    print(1 if sol1.isPerfectSquare(16) else 2)
     
