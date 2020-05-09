c2b1ae8fa980b7447ceacd43867132b7b088a296
EC2 Default User <ec2-user@ip-172-31-41-237.us-east-2.compute.internal>
2020-05-09 22:00:56 +0000

    using the robbed money list to choose rob or not

--- Google-Coding-List/House-Robber.py
+++ Google-Coding-List/House-Robber.py
@@ -3,43 +3,21 @@
 class Solution:
     def rob(self, nums) -> int:     # conquer
         nums_len = len(nums)
-        res_max = 0
+        if nums_len == 0: return 0
+        if nums_len == 1: return nums[0]
+        if nums_len == 2: return max(nums)
         
-        # use i as the middle point for the dividing at the first time
-        # calculate left, right and then pass those var to the divide() method
-        # wait for the result and conquer
-        for i in range(nums_len):
-            left_max = 0
-            right_max = 0
-            if self.canGiveLeft(nums, i): left_max = self.divide(nums[:i-1])
-            if self.canGiveRight(nums, i): right_max = self.divide(nums[i+2:])
-            
-            current_max = left_max + right_max + nums[i]
-            
-            if res_max < current_max: res_max = current_max
-            
-        return res_max
-    
-    def divide(self, nums):    # divide
-        #until...
-        if not len(nums): return 0
-        if len(nums) == 1: return nums[0]
+        robbed_money_until = []
+        robbed_money_until.append(nums[0])
+        robbed_money_until.append(max(nums[0], nums[1]))
         
-        max_idx = nums.index(max(nums))     # this will be the standard
-        left_max = 0
-        right_max = 0
-        
-        if self.canGiveLeft(nums, max_idx): left_max = self.divide(nums[:max_idx-1])
-        if self.canGiveRight(nums, max_idx): right_max = self.divide(nums[max_idx+2:])
-        return nums[max_idx] + left_max + right_max
-
-    def canGiveLeft(self, nums, mid_idx):
-        return True if not (mid_idx == 0 or mid_idx == 1) else False
-    
-    def canGiveRight(self, nums, mid_idx):
-        return True if not (mid_idx == len(nums) - 1 or mid_idx == len(nums) - 2) else False
+        for i in range(2, nums_len):
+            # if the i'th house money plus the amount of the money until i-2'th money is bigger, rob the i'th house,
+            # but if the amount of the money robbed until i-1'th house is bigger, then not to choose to rob
+            robbed_money_until.append(max(nums[i] + robbed_money_until[i-2], robbed_money_until[i-1]))
+        return  robbed_money_until[len(robbed_money_until) - 1]
         
 # if __name__ == "__main__":
 test_lst1 = [200,234,182,111,87,194,221,217,71,162,140,51,81,80,232,193,223,103,139,103]
 sol1 = Solution()
-print(sol1.rob(test_lst1), tmp_lst)
+print(sol1.rob(test_lst1))
