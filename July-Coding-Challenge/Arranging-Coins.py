31f6b48f80b8c4cdb288142f1001e3b93bb01682
pithecuse527 <gihong96@gmail.com>
2020-07-01 17:02:43 +0000

    more easy way to solve problem using difference sequence

--- July-Coding-Challenge/Arranging-Coins.py
+++ July-Coding-Challenge/Arranging-Coins.py
@@ -1,19 +1,11 @@
 class Solution:
     def arrangeCoins(self, n: int) -> int:
-        if n == 0 or n == 1: return n
+        if n == 0: return 0
         
-        # R := row
-        # S1 := the maximum sum of coins up to ith rows
-        # S2 := the next maximum sum right after the S1
-        # find S using difference series formula
-        a1 = 1  # initiate
+        row = 1
+        s = 1
         
-        # using pigeonhole princple
-        for i in range(1,n):
-            S1 = a1 + ((i-1)*(i+2)) // 2
-            S2 = S1 + (i+1)
-            
-            if n == S1: return i        # case1: if n is S1
-            elif n == S2: return i+1    # case2: if n is S2
-            elif n > S1 and n < S2: return i  # case3: if n is between S1 and S2
-            else: continue
+        while s <= n:
+            row += 1
+            s += row
+        return row-1
