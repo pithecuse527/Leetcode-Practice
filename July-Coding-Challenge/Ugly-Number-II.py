bf15a4709a816acd430d586d2b1b59b056784f89
pithecuse527 <gihong96@gmail.com>
2020-07-06 07:32:31 +0000

    fix grammar error

--- July-Coding-Challenge/Ugly-Number-II.py
+++ July-Coding-Challenge/Ugly-Number-II.py
@@ -1,12 +1,12 @@
 class Solution:
     def nthUglyNumber(self, n: int) -> int:
         ugly_num_lst = [1]
-        two, three, five = 0        # track where to multiply
+        two = three = five = 0        # track where to multiply
         
         while len(ugly_num_lst) < n:
-            while ugly[two]*2 <= ugly[-1]: two += 1
-            while ugly[three]*3 <= ugly[-1]: three += 1
-            while ugly[five]*5 <= ugly[-1]: five +=1
-            ugly_num_lst.append(min(ugly[two]*2, ugly[three]*3, ugly[five]*5))  # use the minimun number to append
+            while ugly_num_lst[two]*2 <= ugly_num_lst[-1]: two += 1
+            while ugly_num_lst[three]*3 <= ugly_num_lst[-1]: three += 1
+            while ugly_num_lst[five]*5 <= ugly_num_lst[-1]: five +=1
+            ugly_num_lst.append(min(ugly_num_lst[two]*2, ugly_num_lst[three]*3, ugly_num_lst[five]*5))  # use the minimun number to append
         return ugly_num_lst[-1]
     
