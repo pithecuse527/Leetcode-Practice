11a5f94538d6b51b1332e92544b9053f66157f73
pithecuse527 <gihong96@gmail.com>
2020-07-10 14:48:37 +0900

    add more commnets

--- July-Coding-Challenge/Plus-One.py
+++ July-Coding-Challenge/Plus-One.py
@@ -2,14 +2,15 @@ class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
         digits_to_int = 0
 
+        # find where to add
         for i in range(len(digits) - 1, -1, -1):
-            if digits[i] == 9:
+            if digits[i] == 9:      # if needs to round up
                 digits[i] = 0
-                if i == 0:
+                if i == 0:          # if needs to round up and add more digit
                     digits.insert(0, 1)
                     break
                 continue
-            else:
+            else:       # if normal case, just add one
                 digits[i] += 1
                 break
 
