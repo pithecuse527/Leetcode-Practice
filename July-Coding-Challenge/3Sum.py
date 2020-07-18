b9ec9b92c16456fed839f1a6c149bee5bfff03bd
pithecuse527 <gihong96@gmail.com>
2020-07-18 12:54:50 +0900

    write tentative codes for todo1

--- July-Coding-Challenge/3Sum.py
+++ July-Coding-Challenge/3Sum.py
@@ -1,2 +1,22 @@
-class Solution:
-    def threeSum(self, nums):
+def threeSum(self, nums):
+    dict_of_nums = {num : nums.count(num) for num in nums}      # create hash map first
+    to_return = []
+
+    # for x in dict_of_nums:
+    #     if dict_of_nums[x]:
+    #         dict_of_nums[x] -= 1
+    #         for y in dict_of_nums:
+    #             if dict_of_nums[y]:
+    #                 dict_of_nums[y] -= 1
+    #                 if -(x+y) in dict_of_nums: to_return.append()
+    #
+    #             dict_of_nums[y] += 1
+    #         dict_of_nums[key] += 1
+
+    # todo1: only look forward (lst and dict)
+    for i in range(len(nums) - 1):
+        x = nums[i]
+        if dict_of_nums[x]:
+            dict_of_nums[x] -= 1
+            tmp_nums = [i + 1:]
+            for j in tmp_nums:
